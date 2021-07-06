6d646bfe94eac1056e851e8f4ada00f05bdb9a25
dllopar <damianllopar@gmail.com>
2021-07-06 14:24:16 -0300

    primer commit

---
+++ BellasArtesA/src/ar/edu/unlam/pb2/obras/Catalogo.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.pb2.obras;
+
+public class Catalogo {
+
+	public Integer ontenerCantidadObras() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void agregarObra(ObraDeArte obraDeArte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
